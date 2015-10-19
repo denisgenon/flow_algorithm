baefadaa327fe94236daf93479b14de7b7b5e915
Denis Genon <denisgenon@gmail.com>
2015-10-19 15:46:27 +0200

    dijkstra

--- java/src/solver/PushRelabel.java
+++ java/src/solver/PushRelabel.java
@@ -1,25 +1,75 @@
 package solver;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
+import flowAlgorithm.FlowAlgorithmInstance;
 import object.Node;
 import object.Vertex;
-import flowAlgorithm.FlowAlgorithmInstance;
 
 public class PushRelabel {
 	public FlowAlgorithmInstance instance;
 	public ArrayList<Vertex> actifV = new ArrayList<Vertex>();
 
 	public void preProcess() {
-		// TODO BFS sur tous les noeuds pour mettre leurs h
+		computeDistanceLabel();
 		for(Vertex v : instance.vertices[0].adjacents) {
 			chargeMax(instance.vertices[0],v);
 		}
 		instance.vertices[0].h=instance.E;
 	}
+	
+	public void computeDistanceLabel() {
+		// Faire un Dijkstra à l'envers
+		Vertex[] invertedVertices = new Vertex[instance.V];
+		
+		for (int i = 0; i < instance.vertices.length; i++) {
+			Vertex v = new Vertex(i);
+			invertedVertices[i] = v;
+		}
+		
+		for (Vertex v : instance.vertices) {
+			for (Vertex u : v.adjacents) {
+				// V -5-> U devient V <-5- U dans invertedCapaMatrix
+				invertedVertices[u.id].adjacents.add(new Vertex(v.id));
+			}
+		}
+		
+		//FlowAlgorithmInstance invertedInstance = new FlowAlgorithmInstance(invertedCapaMatrix, instance.vertices, instance.V, instance.E);
+		ArrayList<Vertex> notS = new ArrayList<>();
+		for (Vertex v : invertedVertices) {
+			notS.add(v);
+			v.h = Integer.MAX_VALUE;
+		}
+		invertedVertices[instance.V - 1].h = 0;
+		while (notS.size() > 0) {
+			Vertex i = findMinimumDistance(notS);
+			notS.remove(i);
+			for (Vertex j : i.adjacents) {
+				if (j.h > i.h + 1) { // pas de cout de distance sur les aretes!
+					j.h = i.h + 1;
+				}
+			}
+		}
+		
+		for (int i = 0; i < invertedVertices.length; i++) {
+			instance.vertices[i].h = invertedVertices[i].h;
+		}				
+	}
+	
+	public Vertex findMinimumDistance(ArrayList<Vertex> vertices) {
+		Vertex minVertex = new Vertex(vertices.size());
+		minVertex.h = Integer.MAX_VALUE;
+		for (Vertex v : vertices) {
+			if (v.h <= minVertex.h) {
+				minVertex = v;
+			}
+		}
+		
+		return minVertex;
+	}
 
-	public void process() {
+	public void process(FlowAlgorithmInstance instance) {
+		this.instance = instance;
 		preProcess();
 		while(!actifV.isEmpty()){
 			Vertex elu = actifV.get(0);// on prend un actif
@@ -31,7 +81,7 @@ public class PushRelabel {
 		int hMin=Integer.MAX_VALUE;
 		for(Vertex u : v.adjacents) {
 			hMin=Math.min(hMin, u.h);
-			if (u.h-1==v.h) {
+			if (u.h-1 == v.h) {
 				chargeCapa(v,u,Math.min(v.e,Node.getNode(v.id,u.id,instance.capaMatrix).capa)); // push
 				return;
 			}
@@ -39,43 +89,8 @@ public class PushRelabel {
 		v.h=hMin+1;
 	}
 
-	public void initH(){
-		
-		int[] distances = new int[instance.V];
-		Vertex[] parent = new Vertex[instance.V];
-
-		for (int i = 0; i < instance.V; i++) {
-			distances[i] = Integer.MAX_VALUE;
-			parent[i] = null;
-		}
-
-		LinkedList<Vertex> queue = new LinkedList<>();
-
-		// Sommet ?
-		distances[0] = 0;
-		queue.add(instance.vertices[0]);
-		while (!queue.isEmpty()) {
-			Vertex u = queue.removeFirst();
-			for (Vertex v : u.adjacents) {
-				if (distances[v.id] == Integer.MAX_VALUE) {
-					distances[v.id] = distances[u.id] + 1;
-					parent[v.id] = u;
-					queue.add(v);
-				}
-			}
-		}
-		int index = instance.V-1;
-		int i = 0;
-		while (parent[index] != null && index != 0) {
-			instance.vertices[index].h=i;
-			index = parent[index].id;
-			i++;
-		}
-	}
-
 	public void chargeCapa(Vertex origin, Vertex desti, int capa) {
-
-		Node myT = Node.getNode(origin.id, desti.id,instance.capaMatrix);
+		Node myT = Node.getNode(origin.id, desti.id, instance.capaMatrix);
 		if(myT.capa<=capa) { // On enleve l'arete si la capa dispo est 0
 			Node.removeNode(origin.id, desti.id,instance.capaMatrix);
 			origin.adjacents.remove(desti);
@@ -85,9 +100,9 @@ public class PushRelabel {
 		}
 
 		origin.e-=capa;
-		if(origin.e<=0)actifV.remove(origin);
+		if(origin.e<=0) actifV.remove(origin);
 		desti.e+=capa;
-		if(desti.e>0)actifV.add(desti);
+		if(desti.e>0) actifV.add(desti);
 
 		myT = Node.getNode(desti.id, origin.id,instance.capaMatrix);
 		if(myT==null) { // on crée l'arete si elle n'existe pas
