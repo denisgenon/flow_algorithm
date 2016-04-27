71059d62c515955756461874866978f98b5ca439
Vvelghe <victorvelghe@hotmail.com>
2016-04-27 08:47:01 +0200

    Preparation pour avoir tous les résultats

--- java/src/tests/NewGeneratorInstance.java
+++ java/src/tests/NewGeneratorInstance.java
@@ -9,8 +9,8 @@ import object.Edge;
 
 public class NewGeneratorInstance {
 
-	public static ArrayList<Edge> edges = new ArrayList<Edge>();
-	public static ArrayList<Edge> allEdges = new ArrayList<Edge>();
+	public static ArrayList<Edge> edges;
+	public static ArrayList<Edge> allEdges;
 	public static int V = 1000;
 	public static int capaMax = 10000;
 
@@ -24,64 +24,12 @@ public class NewGeneratorInstance {
 		return array.get(index);
 	}
 
-	public static void generateInstance(int prct) {
-		ArrayList<Integer> connected = new ArrayList<Integer>();
-		ArrayList<Integer> doubleLinked = new ArrayList<Integer>();
+	public static void generateUniqueInstance(String foldername) {
 
-		// On fait une liste de toutes les arêtes possibles
-		for(int u=0; u<V; u++){
-			for(int v=0; v<V;v++){
-				if(u!=v){
-					allEdges.add(new Edge(u,v,capaMax));
-				}
-			}
-		}
+		edges = new ArrayList<Edge>();
+		allEdges = new ArrayList<Edge>();
 
-		// D'abord, avoir un graphe connecté minimum
-		connected.add(0);
-		for(int i=1; i<V; i++){
-			int a = getRandomElem(connected);
-			Edge e = new Edge(a,i,capaMax);
-			Edge reverseE = new Edge(i,a,capaMax);
-			edges.add(e);
-			allEdges.remove(e);
-			allEdges.remove(reverseE);
-			if(a!=0) doubleLinked.add(a);
-			connected.add(i);
-		}
 
-		// Enlever les culs de sacs
-		for(int i=0; i<V; i++){
-			if(!doubleLinked.contains(i)){
-				int a = getRandomElem(connected);
-				Edge e = new Edge(i,a,capaMax);
-				Edge reverseE = new Edge(a,i,capaMax);
-				edges.add(e);
-				allEdges.remove(e);
-				allEdges.remove(reverseE);
-				doubleLinked.add(i);
-			}
-		}
-
-		// Rajouter des arêtes pour avoir le pourcentage voulu
-		int currentprct = (int)((double)(edges.size()-(V-1))/(((V*(V-1))/2)-(V-1))*100);
-		int oldprct = (int)((double)(edges.size()-(V-1))/(((V*(V-1))/2)-(V-1))*100);
-		while((double)(edges.size()-(V-1))/(((V*(V-1))/2)-(V-1))*100 < prct){
-
-			Edge newEdge = getRandomEdge(allEdges);
-			edges.add(newEdge);
-			allEdges.remove(newEdge);
-			allEdges.remove(new Edge(newEdge.v,newEdge.u,capaMax));
-
-			currentprct = (int)((double)(edges.size()-(V-1))/(((V*(V-1))/2)-(V-1))*100);
-			if(currentprct!=oldprct) {
-				System.out.println(currentprct);
-				oldprct=currentprct;
-			}
-		}
-	}
-
-	public static void generateUniqueInstance() {
 		ArrayList<Integer> connected = new ArrayList<Integer>();
 		ArrayList<Integer> doubleLinked = new ArrayList<Integer>();
 
@@ -138,20 +86,20 @@ public class NewGeneratorInstance {
 					oldprct=currentprct;
 				}
 			}
-			
+
 			String pr;
 			if (prct==5)pr="05";
 			else pr=Integer.toString(prct);
 			System.out.println("Création de : "+"instanceuniqueprct"+pr);
-			createGraphFile("instanceuniqueprct"+pr);
+			createGraphFile(foldername, "instanceuniqueprct"+pr);
 		}
 	}
 
-	public static void createGraphFile(String filename) {
-		File f = new File ("instancesUniquePrct3/"+filename+".txt");
+	public static void createGraphFile(String foldername, String filename) {
+		File f = new File ("instances/"+foldername+"/"+filename+".txt");
 		FileWriter fw;
 		try {
-			fw = new FileWriter (f);
+			fw = new FileWriter(f);
 			fw.write(V+" "+edges.size()+"\n");
 
 			for(Edge c : edges){
@@ -166,8 +114,9 @@ public class NewGeneratorInstance {
 	public static void main (String [] args) {	
 		/*generateInstance(65);
 		createGraphFile("instanceprct65");*/
-		
-		generateUniqueInstance();
+		for(int i=2; i<=100; i++){
+			generateUniqueInstance("instancesUniquePrct"+i);
+		}
 
 	}
 }
